da6b976c66309ce393b0ccee7b936b25db26a68c
Mr-Zanzibar <104436099+Mr-Zanzibar@users.noreply.github.com>
2023-03-09 15:55:57 +0100

    2023 update

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 age = int(input("Enter Your birth year: "))
-result = 2022 - age
+result = 2023 - age # 2023 update
 print("You're", +result, "years old")
 print("LMAO") # yes, I wanted to make this very DIFFICULT project public
